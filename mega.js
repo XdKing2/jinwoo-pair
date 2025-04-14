efb082fe4b9a74cb8c430df68276b47278f9b4b1
Malvin King <malvintech2@gmail.com>
2025-04-14 12:37:05 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 
 const auth = {
-    email: 'malvinb017@gmail.com',   //use your real vaild mega account email
+    email: 'malvink003@gmail.com',   //use your real vaild mega account email
     password: 'malvin266',  ////use your real vaild mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
